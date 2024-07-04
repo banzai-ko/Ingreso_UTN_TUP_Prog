1eb2e52778ddabffcdb390eb27aeb70d8e1d217f
genaro14 <genaropennone@gmail.com>
2024-07-04 15:22:02 -0300

    Ejemplo for/while

--- desafios/01_desafio_incremento.py
+++ desafios/01_desafio_incremento.py
@@ -1,6 +1,11 @@
-print("ingresa numero")
-value = int(input())
+value = int(input("ingresa numero: "))
 print ("El numero ingresado fue:", value)
-while value < 100:
+
+for i in range(value,value + 11):
     print(value)
     value += 1
+
+
+# while value <= value + 10 : Usar un for para el incremento en vez de un while
+#     print(value)
+#     value += 1
